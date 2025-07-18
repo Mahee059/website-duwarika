5584d86152752839974c413087ddcd3571363f2f
Mahee059 <munakhatiwada2059@gmail.com>
2025-07-18 17:01:33 +0545

    build error

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='flex flex-col items-center justify-center py-8 text-gray-800 bg-gray-100 space-y-4 mx-auto'>
-      <h2 className='font-extralight text-base'>Nepal's Finest Luxury Boutique Hotel </h2>
+      <h2 className='font-extralight text-base'>Nepal`s Finest Luxury Boutique Hotel </h2>
       <h1 className='font-medium text-2xl tracking-wider'>The Dwarika`s Kathmandu</h1>
       <p className='max-w-[600px] text-center text-xs '>Born out of Mr. Dwarika Das Shrestha`s quest to preserve the Kathmandu Valley`s heritage, Dwarika`s Hotel is a sanctuary for the architectural grandeur of the Newari civilization. The intricately carved wooden treasures, with each artifact being an original, are the soul of the Dwarika`s. Titled as an oasis in the middle of a bustling city, the peaceful courtyards display the grand architectural wooden masterpieces while being overlooked by the meticulously decorated spacious rooms, offering a unique experience deep-rooted in Nepalese culture. The Dwarika`s is many things- a labor of love, a realization of architectural heritage conservation, a place where you are treated like royalty, and never just a luxury boutique hotel. </p>
     </div>
